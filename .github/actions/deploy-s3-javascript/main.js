8fab09d8587ea66456a660514a38c6b49d6bf101
Christopher Steigerwald <cjsteigerwald@gmail.com>
2024-12-23 12:22:01 -0800

    fix

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,7 +6,7 @@ function run() {
 	const bucketRegion = core.getInput('bucket-region', { required: true });
 	const distFolder = core.getInput('dist-folder', { required: true });
 
-	const s3Uri = `s3:\\${bucket}`;
+	const s3Uri = `s3://${bucket}`;
 	exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
 	core.notice('Hello form my custom JavaScript Action!');
