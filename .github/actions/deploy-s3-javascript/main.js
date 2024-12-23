2690a1b9b2e40b4696cdf1239157433762bbace2
Christopher Steigerwald <cjsteigerwald@gmail.com>
2024-12-23 12:18:33 -0800

    fix var naming error

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -7,7 +7,7 @@ function run() {
 	const distFolder = core.getInput('dist-folder', { required: true });
 
 	const s3Uri = `s3:\\${bucket}`;
-	exec.exec(`aws s3 sync ${distFolder} ${s4Uri} --region ${bucketRegion}`);
+	exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
 	core.notice('Hello form my custom JavaScript Action!');
 }
